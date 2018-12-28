0ae0a4a8ac3b52e2e343c8ed5c390f7afba5d66f
ullbergm <magnus@ullberg.us>
2018-12-28 11:57:45 -0500

    Update smtp-relay.sh

--- overlay/smtp-relay.sh
+++ overlay/smtp-relay.sh
@@ -4,13 +4,9 @@ SMTP_RELAY_HOST=${SMTP_RELAY_HOST?Missing env var SMTP_RELAY_HOST}
 SMTP_RELAY_MYHOSTNAME=${SMTP_RELAY_MYHOSTNAME?Missing env var SMTP_RELAY_MYHOSTNAME}
 SMTP_RELAY_USERNAME=${SMTP_RELAY_USERNAME?Missing env var SMTP_RELAY_USERNAME}
 SMTP_RELAY_PASSWORD=${SMTP_RELAY_PASSWORD?Missing env var SMTP_RELAY_PASSWORD}
-
-# handle sasl
 echo "${SMTP_RELAY_HOST} ${SMTP_RELAY_USERNAME}:${SMTP_RELAY_PASSWORD}" > /etc/postfix/sasl_passwd || exit 1
 postmap /etc/postfix/sasl_passwd || exit 1
 rm /etc/postfix/sasl_passwd || exit 1
-
-# These are required.
 postconf "relayhost = ${SMTP_RELAY_HOST}" || exit 1
 postconf "myhostname = ${SMTP_RELAY_MYHOSTNAME}" || exit 1
 
